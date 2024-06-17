4d112e9f478d35130182fe2b165b22745fd2c4dd
Gregor Debus <gdebus@ernw.de>
2024-06-17 14:47:31 +0200

    Added hypervisor table

--- openstack/table_openstack_hypervisor.go
+++ openstack/table_openstack_hypervisor.go
@@ -16,143 +16,9 @@ import (
 func tableOpenstackHypervisor(_ context.Context) *plugin.Table {
 	return &plugin.Table{
 		Name:        "openstack_hypervisor",
-		Description: "OpenStack Hypervisor",
-		Columns: []*plugin.Column{
-			{
-				Name:        "id",
-				Type:        proto.ColumnType_STRING,
-				Description: "The unique id of the hypervisor",
-				Transform:   transform.FromField("ID"),
-			},
-			{
-				Name:        "hypervisor_hostname",
-				Type:        proto.ColumnType_STRING,
-				Description: "The hostname of the hypervisor",
-				Transform:   transform.FromField("HypervisorHostname"),
-			},
-			{
-				Name:        "host_ip",
-				Type:        proto.ColumnType_STRING,
-				Description: "The host_ip of the hypervisor",
-				Transform:   transform.FromField("HostIP"),
-			},
-			{
-				Name:        "state",
-				Type:        proto.ColumnType_STRING,
-				Description: "The state of the hypervisor",
-				Transform:   transform.FromField("State"),
-			},
-			{
-				Name:        "hypervisor_version",
-				Type:        proto.ColumnType_STRING,
-				Description: "The version of the hypervisor",
-				Transform:   transform.FromField("HypervisorVersion"),
-			},
-			{
-				Name:        "vcpus",
-				Type:        proto.ColumnType_INT,
-				Description: "The total number of vcpus on the hypervisor",
-				Transform:   transform.FromField("VCPUs"),
-			},
-			{
-				Name:        "cpu_vendor",
-				Type:        proto.ColumnType_STRING,
-				Description: "The vendor of the CPU",
-				Transform:   transform.FromField("CPUInfo.Vendor"),
-			},
-			{
-				Name:        "cpu_arch",
-				Type:        proto.ColumnType_STRING,
-				Description: "The arch of the CPU",
-				Transform:   transform.FromField("CPUInfo.Arch"),
-			},
-			{
-				Name:        "cpu_model",
-				Type:        proto.ColumnType_STRING,
-				Description: "The model of the CPU",
-				Transform:   transform.FromField("CPUInfo.Model"),
-			},
-			{
-				Name:        "vcpus_used",
-				Type:        proto.ColumnType_INT,
-				Description: "The number of vcpus used on the hypervisor",
-				Transform:   transform.FromField("VCPUsUsed"),
-			},
-			{
-				Name:        "current_workload",
-				Type:        proto.ColumnType_INT,
-				Description: "The current_workload of the hypervisor",
-				Transform:   transform.FromField("CurrentWorkload"),
-			},
-			{
-				Name:        "status",
-				Type:        proto.ColumnType_STRING,
-				Description: "The status of the hypervisor",
-				Transform:   transform.FromField("Status"),
-			},
-			{
-				Name:        "disk_available_least",
-				Type:        proto.ColumnType_INT,
-				Description: "The actual free disk on this hypervisor",
-				Transform:   transform.FromField("DiskAvailableLeast"),
-			},
-			{
-				Name:        "free_disk_on_hypervisor",
-				Type:        proto.ColumnType_INT,
-				Description: "the free disk remaining on the hypervisor, measured in GB",
-				Transform:   transform.FromField("FreeDiskGB"),
-			},
-			{
-				Name:        "free_ram_mb",
-				Type:        proto.ColumnType_INT,
-				Description: "The free RAM in the hypervisor, measured in MB",
-				Transform:   transform.FromField("FreeRamMB"),
-			},
-			{
-				Name:        "local_gb",
-				Type:        proto.ColumnType_INT,
-				Description: "the disk space in the hypervisor, measured in GB",
-				Transform:   transform.FromField("LocalGB"),
-			},
-			{
-				Name:        "local_gb_used",
-				Type:        proto.ColumnType_INT,
-				Description: "The used disk space of the hypervisor",
-				Transform:   transform.FromField("LocalGBUsed"),
-			},
-			{
-				Name:        "running_vms",
-				Type:        proto.ColumnType_INT,
-				Description: "The number of running vms on the hypervisor",
-				Transform:   transform.FromField("RunningVMs"),
-			},
-			{
-				Name:        "memory_mb",
-				Type:        proto.ColumnType_INT,
-				Description: "The total memory of the hypervisor",
-				Transform:   transform.FromField("MemoryMB"),
-			},
-			{
-				Name:        "memory_mb_used",
-				Type:        proto.ColumnType_INT,
-				Description: "The used memory of the hypervisor, measured in MB",
-				Transform:   transform.FromField("MemoryMBUsed"),
-			},
-			{
-				Name:        "service",
-				Type:        proto.ColumnType_STRING,
-				Description: "The service this hypervisor represents",
-				Transform:   transform.FromField("Service"),
-			},
-			{
-				Name:        "hypervisor_type",
-				Type:        proto.ColumnType_STRING,
-				Description: "The type of hypervisor",
-				Transform:   transform.FromField("HypervisorType"),
-			},
-		},
+		Description: "Table of all hypervisors.",
 		List: &plugin.ListConfig{
-			Hydrate: listOpenStackHypervisor,
+			Hydrate: listHypervisor,
 			KeyColumns: plugin.KeyColumnSlice{
 				&plugin.KeyColumn{
 					Name:    "id",
@@ -162,77 +28,90 @@ func tableOpenstackHypervisor(_ context.Context) *plugin.Table {
 		},
 		Get: &plugin.GetConfig{
 			KeyColumns: plugin.SingleColumn("id"),
-			Hydrate:    getOpenStackHypervisor,
+			Hydrate:    getHypervisor,
+		},
+		Columns: []*plugin.Column{
+			{Name: "id", Type: proto.ColumnType_STRING, Description: "The unique ID of the hypervisor."},
+			{Name: "hypervisor_hostname", Type: proto.ColumnType_STRING, Description: "The hostname of the hypervisor."},
+			{Name: "host_ip", Type: proto.ColumnType_STRING, Description: "The hypervisor's IP address.", Transform: transform.FromField("HostIP")},
+			{Name: "state", Type: proto.ColumnType_STRING, Description: "State of the hypervisor, either 'up'or 'down'."},
+			{Name: "hypervisor_version", Type: proto.ColumnType_STRING, Description: "The version of the hypervisor."},
+			{Name: "vcpus", Type: proto.ColumnType_INT, Description: "The total number of vcpus on the hypervisor.", Transform: transform.FromField("VCPUs")},
+			{Name: "cpu_vendor", Type: proto.ColumnType_STRING, Description: "The vendor of the CPU.", Transform: transform.FromField("CPUInfo.Vendor")},
+			{Name: "cpu_arch", Type: proto.ColumnType_STRING, Description: "The arch of the CPU.", Transform: transform.FromField("CPUInfo.Arch")},
+			{Name: "cpu_model", Type: proto.ColumnType_STRING, Description: "The model of the CPU.", Transform: transform.FromField("CPUInfo.Model")},
+			{Name: "vcpus_used", Type: proto.ColumnType_INT, Description: "The number of used vcpus on the hypervisor.", Transform: transform.FromField("VCPUsUsed")},
+			{Name: "current_workload", Type: proto.ColumnType_INT, Description: "The number of tasks the hypervisor is responsible for."},
+			{Name: "status", Type: proto.ColumnType_STRING, Description: "Status of the hypervisor, either 'enabled' or 'disabled'."},
+			{Name: "disk_available_least", Type: proto.ColumnType_INT, Description: "The actual free disk on the hypervisor, measured in GB."},
+			{Name: "free_disk_gb", Type: proto.ColumnType_INT, Description: "The free disk remaining on the hypervisor, measured in GB.", Transform: transform.FromField("FreeDiskGB")},
+			{Name: "free_ram_mb", Type: proto.ColumnType_INT, Description: "The free RAM in the hypervisor, measured in MB.", Transform: transform.FromField("FreeRamMB")},
+			{Name: "local_gb", Type: proto.ColumnType_INT, Description: "The disk space in the hypervisor, measured in GB.", Transform: transform.FromField("LocalGB")},
+			{Name: "local_gb_used", Type: proto.ColumnType_INT, Description: "The used disk space of the hypervisor, measured in GB.", Transform: transform.FromField("LocalGBUsed")},
+			{Name: "running_vms", Type: proto.ColumnType_INT, Description: "The number of running vms on the hypervisor.", Transform: transform.FromField("RunningVMs")},
+			{Name: "memory_mb", Type: proto.ColumnType_INT, Description: "The total memory of the hypervisor, measured in MB.", Transform: transform.FromField("MemoryMB")},
+			{Name: "memory_mb_used", Type: proto.ColumnType_INT, Description: "The used memory of the hypervisor, measured in MB.", Transform: transform.FromField("MemoryMBUsed")},
+			{Name: "service", Type: proto.ColumnType_STRING, Description: "The service this hypervisor represents."},
+			{Name: "hypervisor_type", Type: proto.ColumnType_STRING, Description: "The type of hypervisor."},
 		},
 	}
 }
 
-//// LIST FUNCTION
-
-func listOpenStackHypervisor(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+func listHypervisor(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 
 	logger := plugin.Logger(ctx)
+
 	provider, err := connect(ctx, d)
 	if err != nil {
-		logger.Error("openstack_server.listServer", "connection_error", err)
+		logger.Error("openstack_hypervisor.listHypervisor", "connection_error", err)
 		return nil, err
 	}
 
 	// get compute client from provider
 	endpointOpts := getEndpointOpts(d)
 	computeClient, err := openstack.NewComputeV2(provider, endpointOpts)
-
-	allPages, err := hypervisors.List(computeClient, nil).AllPages()
-
-	logger.Error("all hypervisors", "hypervisors", allPages)
-
 	if err != nil {
+		logger.Error("openstack_hypervisor.listHypervisor", "connection_error", err)
 		return nil, err
 	}
+
+	// get hypervisors
+	allPages, err := hypervisors.List(computeClient, hypervisors.ListOpts{}).AllPages()
+	if err != nil {
+		logger.Error("openstack_hypervisor.listHypervisor", "query_error", err)
+		return nil, err
+	}
+
 	allHypervisors, err := hypervisors.ExtractHypervisors(allPages)
-	if err != nil {
-		return nil, err
+	for _, hypervisor := range allHypervisors {
+		d.StreamListItem(ctx, hypervisor)
 	}
 
-	for _, hypervisor := range allHypervisors {
-		if ctx.Err() != nil {
-			break
-		}
-		hypervisor := hypervisor
-		d.StreamListItem(ctx, &hypervisor)
-	}
 	return nil, nil
 }
 
-//// HYDRATE FUNCTIONS
-
-func getOpenStackHypervisor(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+func getHypervisor(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 
 	logger := plugin.Logger(ctx)
+
+	id := d.EqualsQuals["id"].GetStringValue()
+
 	provider, err := connect(ctx, d)
 	if err != nil {
-		logger.Error("openstack_server.listServer", "connection_error", err)
+		logger.Error("openstack_hypervisor.getHypervisor", "connection_error", err)
 		return nil, err
 	}
 
-	id := d.EqualsQuals["id"].GetStringValue()
 	// get compute client from provider
 	endpointOpts := getEndpointOpts(d)
 	computeClient, err := openstack.NewComputeV2(provider, endpointOpts)
 
-	result := hypervisors.Get(computeClient, id)
-	var hypervisor *hypervisors.Hypervisor
-	hypervisor, err = result.Extract()
+	// get hypervisor
+	hypervisor, err := hypervisors.Get(computeClient, id).Extract()
 	if err != nil {
+		logger.Error("openstack_hypervisor.getHypervisor", "query_error", err)
 		return nil, err
 	}
 
 	return hypervisor, nil
-}
-
-func buildOpenStackHypervisorFilter(ctx context.Context, quals plugin.KeyColumnEqualsQualMap) hypervisors.ListOpts {
-
-	opts := hypervisors.ListOpts{}
-
-	return opts
 }
